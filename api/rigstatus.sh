8d1912aa8efc13c9da6c13b8a75e97a14e98be64
rollmeister <32990848+rollmeister@users.noreply.github.com>
2018-07-25 00:23:02 +0300

    Miner's on LAN status Utility
    chmod u+x rigstatus.sh
    to make script executable
    Updated with optional scan method for WIFI clients with power saving enabled.

--- api/rigstatus.sh
+++ api/rigstatus.sh
@@ -2,8 +2,9 @@
 
 # cpuminer's on network summarizer.
 # Tested with Verium(VRM) v1.4 fireworm71 miner.
-# May work on other miner software...
+# Might work on other miner software...
 # GPL. No warranty or support of any kind.
+# Author rollmeister @ https://vericoinandverium.slack.com
 
 # 30 seconds timeout during probing
 probetimeout=30;
@@ -71,23 +72,6 @@ fi
 
 printf "Press 'CTRL+C' to cancel at any time...\n";
 printf "* Using IP Class\e[33m ${ipclass}\e[0m & Port\e[33m ${port}\e[0m\n";
-#ipaddresslist=`nmap --max-retries=250 -sn ${ipclass}.0-255 -oG - | awk '/Up$/{print $2}';`;
-#ipaddresslist=`nmap --send-ip --unprivileged -sP -n ${ipclass}.0/24 -oG - | awk '/Up$/{print $2}';`;
-#--send-ip --unprivileged -sP 192.168.10.0/24
-#ipaddresslist=`nmap -PA -n ${ipclass}.0/24 -oG - | awk '/Up$/{print $2}';`;
-#-PA -n
-
-#if [[ -z $ipaddresslist ]]; then
-
-#printf "\x1b[33m No valid IP addresses found...\x1b[0m\n";
-#printf "Is Router isolating clients?\n";
-#printf "Note(1). All miners must be run with \x1b[36m--api-bind 0.0.0.0:4048\x1b[0m parameter in their commandline\n";
-#printf "in order for this script to successfuly query the running miner. Change recommended Port if needed.\n";
-#printf "Note(2). WIFI adapters with powers saving enabled, maybe unreliable to probing. You could try -a for alternative scan.\n";
-#printf "\e[1m$0 -h\e[0m for usage instruction (i.e. providing Port number, IP Class)\n";
-#exit 0;
-
-#fi
 
 minercount=0;
 if [ "$altscan" = false ]; then
@@ -105,7 +89,6 @@ printf "\e[1m$0 -h\e[0m for usage instruction (i.e. providing Port number, IP Cl
 exit 0;
 
 fi
-
 	for dest in $ipaddresslist; do
 	summary=`echo summary | nc -s ${probetimeout} -w ${probetimeout} -n ${dest} ${port} 2> /dev/null;`;
 
